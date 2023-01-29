744f0c25db54d4b40c2251d15845ad47c48ad8ed
akhil397 <antonyakhil51@gmail.com>
2023-01-29 22:02:04 +0530

    i solved my error

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/',views.register, name='register'),
     path('login/',views.login, name='login'),
-    path('chage_password/',views.chagepass, name='chagepass')
+    path('password/', views.chagepass, name='chagepass')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ import email
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 
 
@@ -53,10 +53,17 @@ def register(request):
 
 def chagepass(request):
     if request.method == 'POST':
-        chageform =PasswordChangeForm(user=request.user,data=request.POST)
-        if chageform.is_valid:
-            chageform.save()
-            return render('login')
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
     else:
-        chageform = PasswordChangeForm(user=request.user)
-    return render(request, 'registration/changepass.html',{'form':chageform})
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/changepass.html', {
+        'form': form
+    })
+    
